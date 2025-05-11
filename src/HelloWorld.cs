32e8ad822dd85b444cc14a7a4798c6950f341621
AbdulS4amad <4sk415@gmail.com>
2025-05-11 22:49:48 +0500

    Update HelloWorld.cs

--- src/HelloWorld.cs
+++ src/HelloWorld.cs
@@ -51,6 +51,7 @@ namespace MyApp
             PrintElement(myData, 102);
 
             SortArray();
+            GetNthFibonacci_Ite(5);
 
             Logger.Info("Terminating program ...");
         }
@@ -84,6 +85,19 @@ namespace MyApp
                     }
         
             return NumArray;
+        }
+
+        public static int GetNthFibonacci_Ite(int n)
+        {
+            int number = n - 1; //Need to decrement by 1 since we are starting from 0
+            int[] Fib = new int[number + 1];
+            Fib[0]= 0;
+            Fib[1]= 1;
+            for (int i = 2; i <= number;i++)
+            {
+               Fib[i] = Fib[i - 2] + Fib[i - 1];
+            }
+            return Fib[number];
         }
 
     } // class HelloWorld
