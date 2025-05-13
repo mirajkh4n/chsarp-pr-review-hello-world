a5495338ca7e9404f8340cbad4aef0d635e63d60
Abdul Samad <4sk415@gmail.com>
2025-05-13 14:01:07 +0500

    COMMITS

--- src/HelloWorld.cs
+++ src/HelloWorld.cs
@@ -65,43 +65,43 @@ namespace MyApp
             {
                 Logger.Warn("idx {0}: no such element in DataStore", index);
                 Console.WriteLine(String.Format("idx {0}: no such element in DataStore", index));
-            }
 
-            //New code push1
-                    using System;
-
-                class Program
-                {
-                    static void Main()
-                    {
-                        int[] numbers = { 5, 2, 9, 1, 5, 6 };
-                        SortArray(numbers);
-
-                        Console.WriteLine("Sorted array:");
-                        foreach (var num in numbers)
-                        {
-                            Console.Write(num + " ");
-                        }
-                    }
-
-                    static void SortArray(int[] arr)
-                    {
-                        for (int i = 0; i < arr.Length; i++)
-                        {
-                            for (int j = i + 1; j < arr.Length; j++)
+                            
+                            class Program
                             {
-                                if (arr[j] < arr[i])
+                                static void Main()
                                 {
-                                    int temp = arr[j];
-                                    arr[i] = arr[j];
-                                    arr[j] = temp;
+                                    int[] numbers = { 5, 2, 9, 1, 5, 6 };
+                                    SortArray(numbers);
+
+                                    Console.WriteLine("Sorted array:");
+                                    foreach (var num in numbers)
+                                    {
+                                        Console.Write(num + " ");
+                                    }
+                                }
+
+                                static void SortArray(int[] arr)
+                                {
+                                    for (int i = 0; i < arr.Length; i++)
+                                    {
+                                        for (int j = i + 1; j < arr.Length; j++)
+                                        {
+                                            if (arr[j] < arr[i])
+                                            {
+                                                int temp = arr[j];
+                                                arr[i] = arr[j];
+                                                arr[j] = temp;
+                                            }
+                                        }
+                                    }
                                 }
                             }
-                        }
-                    }
-                }
 
-                        }
+            }
+
+            
+        }
 
     } // class HelloWorld
 
